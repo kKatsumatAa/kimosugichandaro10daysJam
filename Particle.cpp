70dd4196d1dea978a58ddb02271dc250f4dbfad4
チャン スギ <junjunchan64@gmail.com>
2022-09-06 23:58:46 +0900

    炎エフェクトの追加

--- Particle.cpp
+++ Particle.cpp
@@ -1,4 +1,5 @@
 #include "Particle.h"
+#include <random>
 
 void Particle::Initialize() {
 	
@@ -18,11 +19,35 @@ void Particle::BurstGenerate(Vec2 pos,int r, int num, int timer, float angle, un
 	}
 }
 
+void Particle::FireGenerate(Vec2 pos, int r)
+{
+	for (int i = 0; i < 10; i++) {
+		//ƒ‰ƒ“ƒ_ƒ€
+		std::random_device seed_gen;
+		std::mt19937_64 engine(seed_gen());
+		std::uniform_real_distribution<float> x(-15.0f, 15.0f);
+		std::uniform_real_distribution<float> y(-10.0f, 10.0f);
+
+		std::unique_ptr<Fire> newFire = std::make_unique<Fire>();
+		newFire->Initialize();
+		newFire->pos_.x = pos.x + x(engine);
+		newFire->pos_.y = pos.y + y(engine);
+		newFire->r_ = r;
+		fire_.push_back(std::move(newFire));
+	}
+}
+
 void Particle::Update()
 {
+	//”ò‚ÑŽU‚é
 	burst_.remove_if([](std::unique_ptr<Burst>& burst) {return burst->isDead_; });
 	for (std::unique_ptr<Burst>& burst : burst_) {
 		burst->Update();
+	}
+	//‰Î
+	fire_.remove_if([](std::unique_ptr<Fire>& fire) {return fire->isDead_; });
+	for (std::unique_ptr<Fire>& fire : fire_) {
+		fire->Update();
 	}
 }
 
@@ -30,5 +55,8 @@ void Particle::Draw()
 {
 	for (std::unique_ptr<Burst>& burst : burst_) {
 		burst->Draw();
+	}
+	for (std::unique_ptr<Fire>& fire : fire_) {
+		fire->Draw();
 	}
 }
