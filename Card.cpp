2bc03e33151f80ff03338f07511caf251d109d8c
LC1A_04_Yuki_iida <k021g1333@g.neec.ac.jp>
2022-09-07 17:20:59 +0900

    カードとりあえず実装

---
+++ Card.cpp
@@ -0,0 +1,68 @@
+#include "Card.h"
+
+
+void Card::Update()
+{
+}
+
+
+//---------------------------------------------------------------
+void AttackCard::Initialize(int cost, int power)
+{
+	this->cost = cost;
+	this->power = power;
+	attribute = Attibute::ATTACK;
+}
+
+//---------------------------------------------------------------
+void AttackCard::Draw(unsigned int texhandle, Vec2 pos)
+{
+	DrawBox(pos.x - cardLength.x / 2, pos.y - cardLength.y,
+		pos.x + cardLength.x / 2, pos.y + cardLength.y, 0xff0101, true);
+}
+
+void AttackCard::Effect(Player* player, Enemy* enemy)
+{
+	enemy->Damage(power);
+}
+
+
+//--------------------------------------------------------------
+void GuardCard::Initialize(int cost, int guardPower)
+{
+	this->cost = cost;
+	this->guardPower = guardPower;
+	attribute = Attibute::GUARD;
+}
+
+void GuardCard::Draw(unsigned int texhandle, Vec2 pos)
+{
+	DrawBox(pos.x - cardLength.x / 2, pos.y - cardLength.y,
+		pos.x + cardLength.x / 2, pos.y + cardLength.y, 0x0001ff, true);
+}
+
+void GuardCard::Effect(Player* player, Enemy* enemy)
+{
+	player->AddGuardPower(guardPower);
+}
+
+
+//-------------------------------------------------------------------
+void BuffCard::Initialize(int cost, int buffPower)
+{
+	this->cost = cost;
+	this->buffPower = buffPower;
+	attribute = Attibute::BUFF;
+}
+
+void BuffCard::Draw(unsigned int texhandle, Vec2 pos)
+{
+	DrawBox(pos.x - cardLength.x / 2, pos.y - cardLength.y,
+		pos.x + cardLength.x / 2, pos.y + cardLength.y, 0x01ff01, true);
+}
+
+void BuffCard::Effect(Player* player, Enemy* enemy)
+{
+	player->AddPower(buffPower);
+}
+
