aa49aa04a3304e1457be3ff300f4d64b2b76f8d9
LC1A_04_Yuki_iida <k021g1333@g.neec.ac.jp>
2022-09-09 16:55:26 +0900

    強攻撃実装

--- Character.cpp
+++ Character.cpp
@@ -17,13 +17,7 @@
 
 void Character::Update()
 {
-	attackTime++;
-
-	if (attackTime >= attackCool + (attackCool * (deBuffPower/3.0f)))
-	{
-		Attack();
-		attackTime = 0;
-	}
+	attackState->Update();
 
 	if (HP <= 0) isDead = true;
 }
@@ -48,12 +42,17 @@ void Character::Draw()
 	}
 
 	DrawRotaGraph2(pos.x, pos.y, 32.0f, 32.0f, scale, angle, texhandle, true);
+
+	attackState->Draw(&texhandle);
 }
 
 
-void Character::Attack()
+void Character::Attack(int power)
 {
 	isAttack = true;
+
+	if (power != 114514) this->power = power;
+	else                 this->power = powertmp;
 }
 
 void Character::Damage(int power)
@@ -61,4 +60,75 @@ void Character::Damage(int power)
 	if (power - guardPower <= 0) return;
 
 	HP -= (power - guardPower);
+}
+
+
+//---------------------------------------------------------------------------
+void Character::ChangeState(AttackState* state)
+{
+	delete this->attackState;
+	this->attackState = state;
+	state->SetChara(this);
+}
+
+
+//-------------------------------------------------------------------------------
+void NormalAttack::Update()
+{
+	chara->AddAttackTimer();
+
+	//’Êí‚±‚¤‚°‚«
+	if (chara->GetAttackTimer() >= chara->GetAttackCool() + (chara->GetAttackCool() * (chara->GetDeBuff() / 3.0f)))
+	{
+		chara->Attack();
+		chara->SetAttackTime(0);
+	}
+
+	//“G‚ÅAhp‚ª”¼•ªˆÈ‰ºA
+	if (chara->GetHP() <= chara->GetHpMAX() / 2 && !chara->GetIsSpecial() && chara->GetAttribute() == ENEMY)
+	{
+		chara->SetIsSpecial(true);
+		chara->ChangeState(new SpecialAttack);
+		
+	}
+}
+
+void NormalAttack::Draw(unsigned int* texhandle)
+{
+
+}
+
+//-------------------------------------------------------------------------------
+void SpecialAttack::Update()
+{
+	specialGauge++;
+
+	DrawFormatString(0, 0, 0xffffff, "\n\n\n\n\n\ngauge%d", specialGauge);
+
+	//ƒQ[ƒW‚¢‚Á‚Ï‚¢‚É‚È‚Á‚½‚ç’Êí‚ÌUŒ‚—Í‚Ì2”{‚ÌUŒ‚
+	if (specialGauge >= specialGaugeMAX)
+	{
+		chara->Attack(chara->GetPower() * 2);
+		chara->SetAttackTime(0);
+
+		chara->ChangeState(new NormalAttack);
+
+		return;
+	}
+
+	//‚ ‚é’ö“x‚Ü‚Å‘Ì—Íí‚ê‚ÎŽ€‚Ê
+	if (chara->GetHP() < chara->GetHpMAX() / 5 && chara->GetIsSpecial() && chara->GetAttribute() == ENEMY)
+	{
+		chara->AddHP(-chara->GetHpMAX() / 3);
+		chara->ChangeState(new NormalAttack);
+	}
+}
+
+void SpecialAttack::Draw(unsigned int* texhandle)
+{
+	DrawBox(chara->GetPos().x - gaugeLength.x / 2, 
+		chara->GetPos().y - gaugeLength.y / 2 - 70,
+		chara->GetPos().x - gaugeLength.x / 2 + gaugeLength.x * (float)((float)specialGauge / (float)specialGaugeMAX),
+		chara->GetPos().y + gaugeLength.y / 2 - 70,
+		0xffffff, true);
 }
